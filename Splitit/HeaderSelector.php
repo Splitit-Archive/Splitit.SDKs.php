0f953445e8bf6551b998a9106cd2b077447e9c11
Ivan <yohney@gmail.com>
2020-03-02 10:45:32 +0100

    New commit from local dev v.1.3.32

--- Splitit/HeaderSelector.php
+++ Splitit/HeaderSelector.php
@@ -56,7 +56,7 @@ class HeaderSelector
         }
 
         $headers['Content-Type'] = $this->selectContentTypeHeader($contentTypes);
-        $headers['Splitit-SDK'] = "PHP-1.3.17";
+        $headers['Splitit-SDK'] = "PHP-1.3.32";
         return $headers;
     }
 
