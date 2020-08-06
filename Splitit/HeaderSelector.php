6c84503c3fb2ebc89e3c33bc692762e1e700b14d
Your Name <you@example.com>
2020-08-06 09:05:57 +0000

    New Commit from Jenkins Build #

--- Splitit/HeaderSelector.php
+++ Splitit/HeaderSelector.php
@@ -56,7 +56,7 @@ class HeaderSelector
         }
 
         $headers['Content-Type'] = $this->selectContentTypeHeader($contentTypes);
-        $headers['Splitit-SDK'] = "PHP-1.5.3";
+        $headers['Splitit-SDK'] = "PHP-1.5.4";
         return $headers;
     }
 
