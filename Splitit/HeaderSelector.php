cd3ba58b80043b302c49a22af68a63838620c910
Jenkins <jenkins@splitit.com>
2022-02-15 07:04:19 +0000

    New Commit from Jenkins Build #17

--- Splitit/HeaderSelector.php
+++ Splitit/HeaderSelector.php
@@ -56,7 +56,7 @@ class HeaderSelector
         }
 
         $headers['Content-Type'] = $this->selectContentTypeHeader($contentTypes);
-        $headers['Splitit-SDK'] = "PHP-1.6.5516";
+        $headers['Splitit-SDK'] = "PHP-1.6.5517";
         return $headers;
     }
 
